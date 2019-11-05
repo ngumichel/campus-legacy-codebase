0d2304646f7da1a29a9867a0abc83a1f8c925ced
xkr225 <yn.michel@gmail.com>
2019-11-05 11:31:51 +0100

    refacto second draft

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,4 +1,5 @@
 package com.gildedrose;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +16,7 @@ public class GildedRose {
         for (int i = 0; i < items.length; i++) {
             logger.debug("Current item: " + items[i].name + ", " + items[i].sellIn + ", " + items[i].quality);
 
-            if(items[i].quality > 50) {
+            if (items[i].quality > 50) {
                 logger.info("Item is legendary. Quality is " + items[i].quality);
             } else {
 
@@ -23,75 +24,73 @@ public class GildedRose {
                         && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     logger.info("Item is not a Brie or Passes");
 
-                    if (items[i].quality > 0) {
-                        items[i].quality--;
-                        logger.info("Decreasing item quality by one. Quality update " + items[i].quality);
+                    qualityUpdate(i, items[i].quality, 1);
+                    logger.info("Decreasing item quality by one. Quality update " + items[i].quality);
 
-                        if (items[i].name.startsWith("Conjured") && items[i].quality > 0) {
-                            items[i].quality--;
-                            logger.info("Decreasing quality by an extra one because conjured. Quality update " + items[i].quality);
-                        }
+                    if (items[i].name.startsWith("Conjured")) {
+                        qualityUpdate(i, items[i].quality, 1);
+                        logger.info("Decreasing quality by an extra one because conjured. Quality update " + items[i].quality);
                     }
+
                 } else {
                     logger.info("Item is a Brie or Passes");
 
-                    if (items[i].quality < 50) {
-                        items[i].quality++;
-                        logger.info("Increase quality by one. Quality update " + items[i].quality);
+                    qualityUpdate(i, items[i].quality, -1);
+                    logger.info("Increase quality by one. Quality update " + items[i].quality);
 
-                        if (items[i].name.startsWith("Conjured") && items[i].quality < 50) {
-                            items[i].quality++;
-                            logger.info("Increase quality by an extra one because conjured. Quality update " + items[i].quality);
-                        }
-
-                        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-
-                            if (items[i].sellIn < 11 && items[i].quality < 50) {
-                                items[i].quality++;
-                                logger.info("Increase backstage passes quality by an extra one because less than 11 days sellIn. Quality update " + items[i].quality);
-                            }
-
-                            if (items[i].sellIn < 6 && items[i].quality < 50) {
-                                items[i].quality++;
-                                logger.info("Increase backstage passes quality by an another extra one because less than 6 days sellIn. Quality update " + items[i].quality);
-                            }
-                        }
+                    if (items[i].name.startsWith("Conjured")) {
+                        qualityUpdate(i, items[i].quality, -1);
+                        logger.info("Increase quality by an extra one because conjured. Quality update " + items[i].quality);
                     }
-                }
 
-                logger.info("Decreasing item sellIn by one");
-                items[i].sellIn--;
-
-                if (items[i].sellIn < 0) {
-                    if (!items[i].name.contains("Aged Brie")) {
-                        if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].quality > 0) {
-                                items[i].quality--;
-                                logger.info("Decreasing item quality by one. Quality update " + items[i].quality);
-
-                                if (items[i].name.startsWith("Conjured") && items[i].quality > 0) {
-                                    items[i].quality--;
-                                    logger.info("Decreasing item quality by an extra one because conjured. Quality update " + items[i].quality);
-                                }
-                            }
-                        } else {
-                            items[i].quality = items[i].quality - items[i].quality;
-                            logger.info("Backstage passes quality is equal to zero because outdated. Quality update " + items[i].quality);
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (items[i].sellIn < 11) {
+                            qualityUpdate(i, items[i].quality, -1);
+                            logger.info("Increase backstage passes quality by an extra one because less than 11 days sellIn. Quality update " + items[i].quality);
                         }
-                    } else {
-                        if (items[i].quality < 50) {
-                            items[i].quality++;
-                            logger.info("Increase quality by another extra one because sellIn negative. Quality update " + items[i].quality);
-
-                            if (items[i].name.startsWith("Conjured") && items[i].quality < 50) {
-                                items[i].quality++;
-                                logger.info("Increase quality by another extra one because sellIn negative and conjured. Quality update " + items[i].quality);
-                            }
+                        if (items[i].sellIn < 6) {
+                            qualityUpdate(i, items[i].quality, -1);
+                            logger.info("Increase backstage passes quality by an another extra one because less than 6 days sellIn. Quality update " + items[i].quality);
                         }
                     }
                 }
             }
+
+            logger.info("Decreasing item sellIn by one");
+            items[i].sellIn--;
+
+            if (items[i].sellIn < 0) {
+                if (!items[i].name.contains("Aged Brie")) {
+                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        qualityUpdate(i, items[i].quality, 1);
+                        logger.info("Decreasing item quality by one. Quality update " + items[i].quality);
+
+                        if (items[i].name.startsWith("Conjured")) {
+                            qualityUpdate(i, items[i].quality, 1);
+                            logger.info("Decreasing item quality by an extra one because conjured. Quality update " + items[i].quality);
+                        }
+                    } else {
+                        items[i].quality = 0;
+                        logger.info("Backstage passes quality is equal to zero because outdated. Quality update " + items[i].quality);
+                    }
+                } else {
+                    qualityUpdate(i, items[i].quality, -1);
+                    logger.info("Increase quality by another extra one because sellIn negative. Quality update " + items[i].quality);
+
+                    if (items[i].name.startsWith("Conjured")) {
+                        qualityUpdate(i, items[i].quality, -1);
+                        logger.info("Increase quality by another extra one because sellIn negative and conjured. Quality update " + items[i].quality);
+                    }
+                }
+            }
             logger.debug("Updated item: " + items[i].name + ", " + items[i].sellIn + ", " + items[i].quality);
+        }
+    }
+
+    public void qualityUpdate(int i, int quality, int update) {
+
+        if (quality > 0 && quality < 50) {
+            this.items[i].quality = quality - update;
         }
     }
 
